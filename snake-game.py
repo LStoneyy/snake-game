5a9e3fe32d6f33b07b9490d1966c34431efad6b0
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-01-22 19:25:54 +0100

    Added imports and first comment

---
+++ snake-game.py
@@ -0,0 +1,9 @@
+"""This is a simple Snake-Clone made entirely in Python using the PyGame
+Library. The code is meant to be as simple as possible so that
+the game can be used as a selfstudy course for pupils of technology
+in German classrooms."""
+
+# We first need to import the used modules, including PyGame
+import pygame
+import random
+import sys
