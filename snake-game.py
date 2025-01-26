14f9abf0a3f545ac3a03881ee1b3a286ca80521d
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-01-26 18:06:45 +0100

    Added window and color and fps initialisation

--- snake-game.py
+++ snake-game.py
@@ -13,3 +13,21 @@ import sys
     our window in pixels."""
 window_size_x = 720
 window_size_y = 480
+
+""" We now need to initialise a window. For this, we initialise PyGame,
+    set a display with the mode of our intended window size and set
+    a caption. """
+pygame.init()
+window = pygame.display.set_mode(window_size_x, window_size_y)
+pygame.display.set_caption("Snake Game - Python")
+
+""" To control the output Frames per Second, we need to
+    initialise a clock controller."""
+fps_controller = pygame.time.Clock()
+
+""" To display colors, we need to define them first, using RGB values. """
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
